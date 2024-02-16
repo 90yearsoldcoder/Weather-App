23449a49cc3cc31ef689185d59f0ced87fa5f1ca
90yearsoldcoder <minty@bu.edu>
2024-02-15 20:33:37 -0500

    reload airbnb eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: { index: "./src/index.js" },
+  mode: 'development',
+  entry: { index: './src/index.js' },
   context: path.resolve(__dirname),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
@@ -15,30 +15,30 @@ module.exports = {
         test: /\.(avif|png|jpg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "images/",
+              name: '[name].[ext]',
+              outputPath: 'images/',
             },
           },
         ],
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/,
-        use: ["html-loader"],
+        use: ['html-loader'],
       },
     ],
   },
   resolve: {},
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
+      template: './src/index.html',
+      filename: 'index.html',
     }),
   ],
 };
